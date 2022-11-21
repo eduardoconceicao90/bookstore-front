071380ae785dd9db1e2f19d618a89d490468a094
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-11-20 21:16:46 -0300

    Implementando o componente LIVRO-CREATE

--- src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Livro } from '../livro.model';
 import { LivroService } from '../livro.service';
 
@@ -16,7 +16,9 @@ export class LivroReadAllComponent implements OnInit {
 
   id_cat: String = ''
 
-  constructor(private service: LivroService, private route: ActivatedRoute) { }
+  constructor(private service: LivroService, 
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
@@ -29,4 +31,7 @@ export class LivroReadAllComponent implements OnInit {
     })
   }
 
+  navegarParaLivroCreate(){
+    this.router.navigate([`categorias/${this.id_cat}/livros/create`]);
+  }
 }
--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { Livro } from './livro.model';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,23 @@ export class LivroService {
 
   baseUrl: String = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
   findAllByCategoria(id_cat: String): Observable<Livro[]>{
     const url = `${this.baseUrl}/livros?categoria=${id_cat}`
     return this.http.get<Livro[]>(url);
+  }
+
+  create(livro: Livro, id_cat: String): Observable<Livro>{
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`
+    return this.http.post<Livro>(url, livro);
+  }
+
+  message(str: String): void {
+    this._snack.open(`${str}`, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000
+    })
   }
 }
